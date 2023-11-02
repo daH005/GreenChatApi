1f5fc151e9b657afac6ec062e08908d447af7b51
daH005 <danil.shevelev.2004@mail.ru>
2023-11-02 19:34:05 +0500

    Подготовил модуль для работы с БД. Прикрутим ORM.

---
+++ db.py
@@ -0,0 +1,21 @@
+# FixMe: Сюда временно складываем сообщения. В дальнейшем прикрутим БД.
+chats: list[dict] = [
+    # 0
+    {
+        'messages': [],
+        'users_ids': [0, 1],
+    },
+    # 1
+    {
+        'messages': [],
+        'users_ids': [0],
+    },
+]
+users: list[dict[str, str]] = [
+    {
+        'name': 'Danil',
+    },
+    {
+        'name': 'Ivan'
+    },
+]
