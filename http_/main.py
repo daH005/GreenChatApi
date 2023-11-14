fa238553358cdaf4c7c2cec1a785cb3dc16e655b
daH005 <danil.shevelev.2004@mail.ru>
2023-11-14 22:41:27 +0500

    [fix] Корректировка статус-кода.

--- http_/main.py
+++ http_/main.py
@@ -34,8 +34,8 @@ def auth_by_username_and_password() -> AuthTokenJSONDict:
     # Проводим авторизацию.
     try:
         auth_user: User = User.auth_by_username_and_password(username, password)
-    except PermissionError:
-        return abort(HTTPStatus.FORBIDDEN)
+    except ValueError:
+        return abort(HTTPStatus.NOT_FOUND)
     # Возвращаем токен для дальнейшего его сохранения у клиента в cookie.
     return JSONDictPreparer.auth_token(user=auth_user)
 
