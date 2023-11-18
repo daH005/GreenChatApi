f3db7c6dcf1ef91bc1964f1de4d6fd5331ebef3a
daH005 <danil.shevelev.2004@mail.ru>
2023-11-18 19:52:38 +0500

    Переписал тесты для моделей. Семантика следующая - проверяем набор атрибутов.

--- db/_tests/test_models.py
+++ db/_tests/test_models.py
@@ -1,69 +1,59 @@
 import pytest
-from mock_alchemy.mocking import UnifiedAlchemyMagicMock
-from mock_alchemy.comparison import mock
 
-from api.db import models
 from api.db.models import *
 
-_TEST_PASSWORDS = [
-    '901239-18fsdgd0sf7g===23421341',  # noqa
-    '324-fdsg-df-g-345345',  # noqa
-]
-_TEST_USERS = [
-    User.new_by_password(
-        username='dan005',
-        first_name='Данил',
-        last_name='Шевелёв',
-        email='danil.shevelev.2004@mail.ru',
-        password=_TEST_PASSWORDS[0],  # noqa
-    ),
-    User.new_by_password(
-        username='skeletonik',  # noqa
-        first_name='Данила',
-        last_name='Крохалев',
-        email='test_email.2003@mail.ru',
-        password=_TEST_PASSWORDS[1],  # noqa
-    ),
-]
-mock_session: UnifiedAlchemyMagicMock = UnifiedAlchemyMagicMock(data=[
-    # Настраиваем тестовые фильтрации по токену, чтобы они выдавали ожидаемых пользователей.
-    (
-        [mock.call.filter(User.auth_token == _TEST_USERS[0].auth_token)],
-        [_TEST_USERS[0]]
-     ),
-    (
-        [mock.call.filter(User.auth_token == _TEST_USERS[1].auth_token)],
-        [_TEST_USERS[1]]
-    ),
+
+@pytest.mark.parametrize('attr_name', [
+    'id',
+    'username',
+    'first_name',
+    'last_name',
+    'email',
+    'auth_token',
+    'new_by_password',
+    'auth_by_username_and_password',
+    'auth_by_token',
 ])
+def test_user_has_required_attrs(attr_name: str) -> None:
+    """Тест: модель пользователя должна иметь необходимый перечень атрибутов."""
+    assert hasattr(User, attr_name)
 
 
-def setup() -> None:
-    models.session = mock_session
-    mock_session.add_all(_TEST_USERS)
-    mock_session.commit()
+@pytest.mark.parametrize('attr_name', [
+    'id',
+    'name',
+    'messages',
+    'last_message',
+])
+def test_chat_has_required_attrs(attr_name: str) -> None:
+    """Тест: модель чата должна иметь необходимый перечень атрибутов."""
+    assert hasattr(Chat, attr_name)
 
 
-@pytest.mark.parametrize('user', _TEST_USERS)
-def test_user_creating_with_auth_token_generating_and_no_password_saving(user: User) -> None:
-    """Тест: у созданных пользователей должен быть токен авторизации и не
-    должно быть чистого пароля.
-    """
-    assert hasattr(user, 'auth_token')
-    assert not hasattr(user, 'password')
+@pytest.mark.parametrize('attr_name', [
+    'id',
+    'user_id',
+    'chat_id',
+    'user',
+    'text',
+    'creating_datetime',
+])
+def test_chat_message_has_required_attrs(attr_name: str) -> None:
+    """Тест: модель сообщения должна иметь необходимый перечень атрибутов."""
+    assert hasattr(ChatMessage, attr_name)
 
 
-@pytest.mark.parametrize(('user', 'password'), zip(_TEST_USERS, _TEST_PASSWORDS))
-def test_auth_user_by_username_and_password(user: User,
-                                            password: str,
-                                            ) -> None:
-    """Тест: авторизация по логину и паролю должна работать.
-    """
-    assert User.auth_by_username_and_password(user.username, password) == user
-
-
-@pytest.mark.parametrize('user', _TEST_USERS)
-def test_auth_user_by_token(user: User) -> None:
-    """Тест: авторизация по токену должна работать.
-    """
-    assert User.auth_by_token(user.auth_token) == user
+@pytest.mark.parametrize('attr_name', [
+    'id',
+    'user_id',
+    'chat_id',
+    'user',
+    'chat',
+    'chat_if_user_has_access',
+    'users_in_chat',
+    'user_chats',
+    'chat_name',
+])
+def test_user_chat_match_has_required_attrs(attr_name: str) -> None:
+    """Тест: модель доступа к чату должна иметь необходимый перечень атрибутов."""
+    assert hasattr(UserChatMatch, attr_name)
