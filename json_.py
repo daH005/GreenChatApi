c6f1c1b2d05bd78520d9b53d1bced05d801d90ec
daH005 <danil.shevelev.2004@mail.ru>
2023-12-11 22:33:17 +0500

    [fix] аннотация

--- json_.py
+++ json_.py
@@ -55,7 +55,7 @@ class WebSocketMessageJSONDict(TypedDict):
     """Тип, определяющий интерфейс словаря-сообщения для веб-сокета."""
 
     type: str
-    data: JWTTokenJSONDict | WebSocketNewChatMessageJSONDict | ChatMessageJSONDict
+    data: JWTTokenJSONDict | WebSocketNewChatMessageJSONDict | WebSocketNewChatJSONDict
 
 
 class WebSocketNewChatMessageJSONDict(TypedDict):
