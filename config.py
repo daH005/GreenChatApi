de9a9d9c7498b7d8a1b9b730c3ef71499eef0348
daH005 <danil.shevelev.2004@mail.ru>
2023-12-15 10:05:14 +0500

    Расставил пробелы. Долгожданное решение.

--- config.py
+++ config.py
@@ -20,24 +20,34 @@ __all__ = (
 )
 
 load_dotenv()
+
 # Абсолютный путь к папке проекта `api`.
 BASE_DIR: Path = Path(__file__).resolve().parent
+
 # Флаг для режима тестирования.
 DEBUG: Final[bool] = False if environ['DEBUG'].lower() == 'false' else bool(environ['DEBUG'])
+
 # Хост общий для HTTP и WebSocket.
 HOST: Final[str] = environ['HOST']
+
 # Порт для REST api.
 HTTP_PORT: Final[int] = int(environ['HTTP_PORT'])
+
 # Порт для веб-сокета.
 WEBSOCKET_PORT: Final[int] = int(environ['WEBSOCKET_PORT'])
+
 # Домены для CORS, с которых мы принимаем запросы.
 CORS_ORIGINS: Final[list[str]] = environ['CORS_ORIGINS'].split(',')
+
 # JWT-токен для REST api.
 JWT_SECRET_KEY: Final[str] = environ['JWT_SECRET_KEY']
+
 # JWT-алгоритм для шифрования и дешифрования.
 JWT_ALGORITHM: Final[str] = environ['JWT_ALGORITHM']
+
 # Срок жизни каждого JWT-токена (в секундах).
 JWT_ACCESS_TOKEN_EXPIRES: Final[int] = int(environ['JWT_ACCESS_TOKEN_EXPIRES'])
+
 # URL для подключения к БД.
 DB_URL: URL = URL.create(
     drivername=environ['DB_DRIVERNAME'],
@@ -47,8 +57,10 @@ DB_URL: URL = URL.create(
     port=int(environ['DB_PORT']),
     database=environ['DB_NAME'],
 )
+
 # Почта для отправки кодов при регистрации.
 EMAIL: Final[str] = environ['EMAIL']
+
 # Пароль для входа в почту (это специальный пароль для приложений).
 EMAIL_PASSWORD: Final[str] = environ['EMAIL_PASSWORD']
 
