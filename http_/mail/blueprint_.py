f482e397cf2b77d36b6ad5a65b870b480b3f00b4
daH005 <danil.shevelev.2004@mail.ru>
2023-12-15 12:31:34 +0500

    Добавил эндпойнт '/user/new/code/check' для проверки кода.

--- http_/mail/blueprint_.py
+++ http_/mail/blueprint_.py
@@ -6,7 +6,7 @@ from typing import Final
 from random import randint
 
 from api.http_.endpoints import EndpointName, Url
-from api.json_ import JSONKey
+from api.json_ import JSONKey, CodeIsValidFlagJSONDict, JSONDictPreparer
 from api.http_.redis_ import app as redis_app
 from api.http_.mail.tasks import send_code_task
 
@@ -30,6 +30,15 @@ def make_and_save_code() -> int:
     return code
 
 
+def check_and_delete_code(code: int | str) -> None:
+    """Проверяет наличие `code` в базе Redis.
+    Если код имеется, то он удаляется. Если кода нет - вызывается `ValueError`.
+    """
+    if not redis_app.sismember(REDIS_SET_NAME, str(code)):
+        raise ValueError
+    redis_app.srem(REDIS_SET_NAME, str(code))
+
+
 @bp.route(Url.SEND_CODE, endpoint=EndpointName.SEND_CODE, methods=[HTTPMethod.POST])
 def send_code() -> dict[Literal['code'], int]:
     """Генерирует случайный код и отправляет его на указанную почту.
@@ -43,3 +52,23 @@ def send_code() -> dict[Literal['code'], int]:
     code: int = make_and_save_code()
     send_code_task.delay(to=email, code=code)
     return dict(code=HTTPStatus.OK)
+
+
+@bp.route(Url.CHECK_CODE, endpoint=EndpointName.CHECK_CODE, methods=[HTTPMethod.POST])
+def check_code() -> CodeIsValidFlagJSONDict:
+    """Проверяет код подтверждения почты. Возвращает словарь с флагом,
+    обозначающим верен ли переданный код.
+    Если код верен, то он удаляется из Redis.
+    Ожидается JSON с одним ключом - 'code'.
+    """
+    try:
+        code: int = int(request.json[JSONKey.CODE])
+    except (ValueError, KeyError):
+        return abort(HTTPStatus.BAD_REQUEST)
+    try:
+        check_and_delete_code(code)
+    except ValueError:
+        flag = False
+    else:
+        flag = True
+    return JSONDictPreparer.prepare_code_is_valid(flag=flag)
