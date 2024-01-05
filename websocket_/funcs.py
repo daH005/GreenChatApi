60851348e414b54a92c1522ccd38112e583fb58a
danil <danil.shevelev.2004@mail.ru>
2024-01-05 23:14:33 +0500

    Написал функцию `make_chat_message_and_add_to_session` для устранения повторов.

--- websocket_/funcs.py
+++ websocket_/funcs.py
@@ -1,13 +1,19 @@
 from re import sub
 from typing import Final
 
-from api.db.models import UserChatMatch
+from api.raises_hinting import raises
+from api.db.models import (
+    session,
+    UserChatMatch,
+    ChatMessage,
+)
 
 __all__ = (
     'UserID',
     'TEXT_MAX_LENGTH',
     'clear_text_message',
     'users_ids_of_chat_by_id',
+    'make_chat_message_and_add_to_session',
 )
 
 UserID = int
@@ -24,6 +30,25 @@ def clear_text_message(text: str) -> str:
 
 def users_ids_of_chat_by_id(chat_id: int) -> list[UserID]:
     return [user.id for user in UserChatMatch.users_in_chat(chat_id=chat_id)]
+
+
+@raises(ValueError)
+def make_chat_message_and_add_to_session(text: str,
+                                         user_id: int,
+                                         chat_id: int,
+                                         ) -> ChatMessage:
+    text = clear_text_message(text=text)
+    if not text:
+        raise ValueError
+
+    chat_message: ChatMessage = ChatMessage(
+        user_id=user_id,
+        chat_id=chat_id,
+        text=text,
+    )
+    session.add(chat_message)
+
+    return chat_message
 
 
 if __name__ == "__main__":
