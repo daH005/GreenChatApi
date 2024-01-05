7c86877b3db895173274cac0939d8c29e8c9374e
danil <danil.shevelev.2004@mail.ru>
2024-01-05 19:43:14 +0500

    Написал модуль `websocket_.base` с основными программными сущностями - `WebSocketServer` и `WebSocketClientHandler`.

---
+++ websocket_/base.py
@@ -0,0 +1,152 @@
+from __future__ import annotations
+import asyncio
+from websockets import WebSocketServerProtocol, serve, ConnectionClosed  # pip install websockets
+from typing import NoReturn, Callable
+import json
+from json import JSONDecodeError
+from jwt import decode  # pip install pyjwt
+
+from api.db.models import User, session
+from api.json_ import JSONKey, WebSocketMessageJSONDict
+from api.raises_hinting import raises
+from api.config import JWT_SECRET_KEY, JWT_ALGORITHM
+from api.websocket_.funcs import UserID
+
+__all__ = (
+    'WebSocketServer',
+    'WebSocketClientHandler',
+    'HandlerFuncT',
+    'HandlerFuncReturnT',
+)
+
+HandlerFuncReturnT = tuple[list[UserID], dict]
+HandlerFuncT = Callable[[User, dict], HandlerFuncReturnT]
+
+
+class WebSocketServer:
+
+    def __init__(self, host: str,
+                 port: int,
+                 ) -> None:
+        self.host = host
+        self.port = port
+        self._clients: dict[UserID, list[WebSocketClientHandler]] = {}
+        self.handlers: dict[str, HandlerFuncT] = {}
+
+    def run(self) -> NoReturn:
+        asyncio.run(self._run())
+
+    async def _run(self) -> NoReturn:
+        async with serve(ws_handler=self._handler, host=self.host, port=self.port):
+            await asyncio.Future()  # run forever
+
+    async def _handler(self, protocol: WebSocketServerProtocol) -> None:
+        try:
+            await self._handle_protocol(protocol)
+        except (ConnectionClosed, ValueError):
+            return
+
+    async def _handle_protocol(self, protocol: WebSocketServerProtocol) -> None:
+        client = WebSocketClientHandler(self, protocol)
+        await client.auth()
+
+        self._add_client(client)
+
+        try:
+            await client.listen()
+        except ConnectionClosed:
+            self._del_client(client)
+            raise
+
+    def _add_client(self, client: WebSocketClientHandler) -> None:
+        self._clients.setdefault(client.user.id, []).append(client)
+
+    def _del_client(self, client: WebSocketClientHandler) -> None:
+        try:
+            self._clients[client.user.id].remove(client)
+        except (KeyError, ValueError):
+            return
+
+    async def send_to_many_users(self, users_ids: list[UserID],
+                                 message: WebSocketMessageJSONDict,
+                                 ) -> None:
+        for id_ in users_ids:
+            if id_ not in self._clients:
+                continue
+            await self.send_to_one_user(id_, message)
+
+    async def send_to_one_user(self, user_id: UserID,
+                               message: WebSocketMessageJSONDict,
+                               ) -> None:
+        for client in self._clients[user_id]:
+            await self.send_to_one_client(client, message)
+
+    @staticmethod
+    async def send_to_one_client(client: WebSocketClientHandler,
+                                 message: WebSocketMessageJSONDict,
+                                 ) -> None:
+        try:
+            await client.protocol.send(json.dumps(message))
+        except ConnectionClosed:
+            return
+
+    def handler(self, type_: str) -> Callable[[HandlerFuncT], HandlerFuncT]:
+        def func_decorator(func: HandlerFuncT) -> HandlerFuncT:
+            self.handlers[type_] = func
+            return func
+        return func_decorator
+
+
+class WebSocketClientHandler:
+
+    def __init__(self, server: WebSocketServer,
+                 protocol: WebSocketServerProtocol,
+                 ) -> None:
+        self.server = server
+        self.protocol = protocol
+        self.user: User | None = None
+
+    @raises(ConnectionClosed, ValueError)
+    async def auth(self) -> None:
+        jwt_token: str = await self._wait_str()
+        auth_token: str = self._decode_jwt(encoded=jwt_token)
+        self._try_auth_user(auth_token=auth_token)
+
+    @staticmethod
+    def _decode_jwt(encoded: str) -> str:
+        return decode(
+            encoded,
+            key=JWT_SECRET_KEY,
+            algorithms=[JWT_ALGORITHM],
+        )['sub']
+
+    @raises(ValueError)
+    def _try_auth_user(self, auth_token: str) -> None:
+        self.user = User.auth_by_token(auth_token=auth_token)
+
+    @raises(ConnectionClosed)
+    async def _wait_str(self) -> str:
+        return await self.protocol.recv()
+
+    @raises(ConnectionClosed, JSONDecodeError)
+    async def _wait_json_dict(self) -> WebSocketMessageJSONDict:
+        return json.loads(await self._wait_str())
+
+    @raises(ConnectionClosed)
+    async def listen(self) -> None:
+        while True:
+            try:
+                message: WebSocketMessageJSONDict = await self._wait_json_dict()
+                self._handle_message(message)
+            except (JSONDecodeError, KeyError, Exception):
+                continue
+            session.remove()
+    
+    @raises(JSONDecodeError, KeyError, Exception)
+    def _handle_message(self, message: WebSocketMessageJSONDict) -> None:
+        users_ids, data = self.server.handlers[message[JSONKey.TYPE]](self.user, message[JSONKey.DATA])  # type: ignore
+        _message = {
+            JSONKey.TYPE: message[JSONKey.TYPE],  # type: ignore
+            JSONKey.DATA: data,
+        }
+        self.server.send_to_many_users(users_ids=users_ids, message=_message)
