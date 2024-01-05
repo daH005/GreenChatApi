377b09869ae755052abeb94ac5b88cc4ff63357f
danil <danil.shevelev.2004@mail.ru>
2024-01-05 23:34:50 +0500

    Разборки с `max_length` + ручные тесты..

--- http_/validation.py
+++ http_/validation.py
@@ -1,5 +1,12 @@
 from __future__ import annotations
-from pydantic import BaseModel, constr, Field, EmailStr, ValidationError
+from pydantic import (
+    BaseModel,
+    constr,
+    Field,
+    validate_email,
+    field_validator,
+    ValidationError,
+)
 from flask import request, abort
 from http import HTTPStatus
 
@@ -12,7 +19,6 @@ __all__ = (
 
 
 class RequestDataHandlerMixin(BaseModel):
-    """Базовый класс, позволяющий сразу получать провалидированные данные из запросов."""
 
     @classmethod
     def from_json(cls) -> RequestDataHandlerMixin | None:
@@ -23,11 +29,28 @@ class RequestDataHandlerMixin(BaseModel):
 
 
 class UserJSONValidator(RequestDataHandlerMixin):
-    """Модель для валидации JSON с данными для создания нового пользователя."""
 
-    username: constr(min_length=1)
-    password: constr(min_length=1)
-    first_name: constr(min_length=1) = Field(alias=JSONKey.FIRST_NAME)
-    last_name: constr(min_length=1) = Field(alias=JSONKey.LAST_NAME)
-    email: EmailStr
+    username: constr(min_length=5, max_length=100)
+    password: constr(min_length=10, max_length=50)
+    first_name: constr(min_length=1, max_length=100) = Field(alias=JSONKey.FIRST_NAME)
+    last_name: constr(min_length=1, max_length=100) = Field(alias=JSONKey.LAST_NAME)
+    email: constr(min_length=1, max_length=200)
     code: int
+
+    @classmethod
+    @field_validator('email')
+    def email_validation(cls, email: str) -> str:
+        return validate_email(email)[1]
+
+
+if __name__ == '__main__':
+    user = UserJSONValidator(
+        username='dan005',
+        password='sadasdsad12313',
+        firstName='danil',
+        lastName='yan',
+        email='dan@mail.ru',
+        code=123
+    )
+    from pprint import pprint
+    pprint(user.model_dump())
