2ddd9ca8c81ea2707b3eb3fabb2f193f263dc24e
danil <danil.shevelev.2004@mail.ru>
2024-01-20 10:35:45 +0500

    Отключил логи `alembic`, чтобы они не перекрывали логи сокета

--- db/alembic_/init.py
+++ db/alembic_/init.py
@@ -1,5 +1,6 @@
 from alembic.config import Config
 from alembic import command
+import logging
 
 from api.config import BASE_DIR, DB_URL
 
@@ -7,6 +8,8 @@ __all__ = (
     'config',
     'make_migrations',
 )
+
+logging.getLogger('alembic.runtime.migration').disabled = True
 
 config: Config = Config(BASE_DIR.joinpath('./db/alembic_/alembic.ini'))
 config.set_main_option('script_location', str(BASE_DIR.joinpath('./db/alembic_/migrations')))
