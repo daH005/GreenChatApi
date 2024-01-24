600f2ab916f0756ed0e306772cc3f82be4ae85f2
danil <danil.shevelev.2004@mail.ru>
2024-01-24 12:07:52 +0500

    Добавил проверку пробелов и переносов на все поля данных нового пользователя (за исключением кода, который представляет собой число, а не строку)

--- http_/validation.py
+++ http_/validation.py
@@ -30,17 +30,28 @@ class RequestDataHandlerMixin(BaseModel):
 
 class UserJSONValidator(RequestDataHandlerMixin):
 
-    username: constr(min_length=5, max_length=100)
-    password: constr(min_length=10, max_length=50)
     first_name: constr(min_length=1, max_length=100) = Field(alias=JSONKey.FIRST_NAME)
     last_name: constr(min_length=1, max_length=100) = Field(alias=JSONKey.LAST_NAME)
+    username: constr(min_length=5, max_length=100)
+    password: constr(min_length=10, max_length=50)
     email: constr(min_length=1, max_length=200)
     code: int
 
+    @field_validator('first_name', 'last_name', 'username', 'password', 'email')  # noqa: from pydantic doc
     @classmethod
-    @field_validator('email')
-    def email_validation(cls, email: str) -> str:
-        return validate_email(email)[1]
+    def _validate_many(cls, data: str) -> str:
+        cls._check_spaces_and_line_breaks_in_str(data)
+        return data
+
+    @staticmethod
+    def _check_spaces_and_line_breaks_in_str(str_: str) -> None:
+        if ' ' in str_ or '\n' in str_:
+            raise AssertionError('All fields (except code) must not contain spaces or line breaks')
+
+    @field_validator('email')  # noqa: from pydantic doc
+    @classmethod
+    def _validate_email(cls, data: str) -> str:
+        return validate_email(data)[1]
 
 
 if __name__ == '__main__':
