354326a4b138e02b0bd9be6cb72245e1eb7c696b
danil <danil.shevelev.2004@mail.ru>
2024-01-26 15:12:02 +0500

    Добавил функцию `make_revision`

--- db/alembic_/init.py
+++ db/alembic_/init.py
@@ -5,6 +5,7 @@ from api.config import BASE_DIR, DB_URL
 
 __all__ = (
     'config',
+    'make_revision',
     'make_migrations',
 )
 
@@ -12,6 +13,13 @@ config: Config = Config(BASE_DIR.joinpath('./db/alembic_/alembic.ini'))
 config.set_main_option('script_location', str(BASE_DIR.joinpath('./db/alembic_/migrations')))
 config.set_main_option('prepend_sys_path', str(BASE_DIR.joinpath('../')))
 config.set_main_option('sqlalchemy.url', DB_URL.render_as_string(hide_password=False))
+
+
+def make_revision(message: str):
+    try:
+        command.revision(config, message, autogenerate=True)
+    except Exception as e:
+        print(e)
 
 
 def make_migrations():
@@ -22,4 +30,5 @@ def make_migrations():
 
 
 if __name__ == '__main__':
+    make_revision(input('Enter your revision message - '))
     make_migrations()
