a2e403c46558e9d1d54006a4aa9776439f0f5f43
danil <danil.shevelev.2004@mail.ru>
2024-02-24 21:53:24 +0500

    Заполнил тестовыми данными константу `NEW_CHAT_HANDLER_SENDINGS` (пока падает исключение, правим)

--- _tests/websocket_test_data.py
+++ _tests/websocket_test_data.py
@@ -100,7 +100,81 @@ ONLINE_STATUS_TRACING_ADDING_HANDLER_SENDINGS = [
 ]
 
 NEW_CHAT_HANDLER_SENDINGS = [
-
+    (
+        dict(
+            user=USERS[1],
+            data={
+                'usersIds': [1, 5],
+                'text': 'Hi!',
+            }
+        ),
+        {
+            1: [
+                {'type': 'newChat',
+                 'data': {
+                     'id': 5,
+                     'name': None,
+                     'isGroup': False,
+                     'lastMessage': {
+                         'id': 5,
+                         'chatId': 5,
+                         'text': 'Hi!',
+                         'creatingDatetime': 0,
+                         'user': {
+                             'id': 1,
+                             'firstName': USERS[1].first_name,
+                             'lastName': USERS[1].last_name,
+                         }
+                     },
+                     'users': [
+                         {
+                             'id': 1,
+                             'firstName': USERS[1].first_name,
+                             'lastName': USERS[1].last_name,
+                         },
+                         {
+                             'id': 5,
+                             'firstName': USERS[5].first_name,
+                             'lastName': USERS[5].last_name,
+                         },
+                     ]
+                 }
+                 }
+            ],
+            5: [
+                {'type': 'newChat',
+                 'data': {
+                     'id': 5,
+                     'name': None,
+                     'isGroup': False,
+                     'lastMessage': {
+                         'id': 5,
+                         'chatId': 5,
+                         'text': 'Hi!',
+                         'creatingDatetime': 0,
+                         'user': {
+                             'id': 1,
+                             'firstName': USERS[1].first_name,
+                             'lastName': USERS[1].last_name,
+                         }
+                     },
+                     'users': [
+                         {
+                             'id': 1,
+                             'firstName': USERS[1].first_name,
+                             'lastName': USERS[1].last_name,
+                         },
+                         {
+                             'id': 5,
+                             'firstName': USERS[5].first_name,
+                             'lastName': USERS[5].last_name,
+                         },
+                     ]
+                 }
+                 }
+            ],
+        }
+    )
 ]
 
 NEW_CHAT_MESSAGE_HANDLER_SENDINGS = [
