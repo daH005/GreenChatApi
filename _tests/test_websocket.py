c336b2d05d499ddaa51ea486f59fbba3e77b4435
danil <danil.shevelev.2004@mail.ru>
2024-02-24 21:18:57 +0500

    Сделал тестовые функции и соответствующие им (пока что пустые) константы

--- _tests/test_websocket.py
+++ _tests/test_websocket.py
@@ -16,7 +16,6 @@ def setup_module() -> None:
 
 
 def _replace_send_to_one_user_method_for_check_data_to_send() -> None:
-
     async def method(user_id: int,
                      message: dict,
                      ) -> None:
@@ -26,7 +25,6 @@ def _replace_send_to_one_user_method_for_check_data_to_send() -> None:
 
 
 def _replace_user_have_one_connection_method_for_online_imitation() -> None:
-
     def method(user_id: int) -> bool:
         return user_id in ONLINE_USERS_IDS
 
@@ -48,3 +46,43 @@ async def test_positive_first_connection_handler(handler_kwargs: dict,
                                                  expected_sendings,
                                                  ) -> None:
     await _test_positive_handler_and_sendings(first_connection_handler, handler_kwargs, expected_sendings)
+
+
+@pytest.mark.parametrize(('handler_kwargs', 'expected_sendings'), FULL_DISCONNECTION_HANDLER_SENDINGS)
+@pytest.mark.asyncio
+async def test_positive_full_disconnection_handler(handler_kwargs: dict,
+                                                   expected_sendings,
+                                                   ) -> None:
+    await _test_positive_handler_and_sendings(full_disconnection_handler, handler_kwargs, expected_sendings)
+
+
+@pytest.mark.parametrize(('handler_kwargs', 'expected_sendings'), ONLINE_STATUS_TRACING_ADDING_HANDLER_SENDINGS)
+@pytest.mark.asyncio
+async def test_positive_online_status_tracing_adding_handler(handler_kwargs: dict,
+                                                             expected_sendings,
+                                                             ) -> None:
+    await _test_positive_handler_and_sendings(new_interlocutor_online_status_adding, handler_kwargs, expected_sendings)
+
+
+@pytest.mark.parametrize(('handler_kwargs', 'expected_sendings'), NEW_CHAT_HANDLER_SENDINGS)
+@pytest.mark.asyncio
+async def test_positive_new_chat_handler(handler_kwargs: dict,
+                                         expected_sendings,
+                                         ) -> None:
+    await _test_positive_handler_and_sendings(new_chat, handler_kwargs, expected_sendings)
+
+
+@pytest.mark.parametrize(('handler_kwargs', 'expected_sendings'), NEW_CHAT_MESSAGE_HANDLER_SENDINGS)
+@pytest.mark.asyncio
+async def test_positive_new_chat_message_handler(handler_kwargs: dict,
+                                                 expected_sendings,
+                                                 ) -> None:
+    await _test_positive_handler_and_sendings(new_chat_message, handler_kwargs, expected_sendings)
+
+
+@pytest.mark.parametrize(('handler_kwargs', 'expected_sendings'), NEW_CHAT_MESSAGE_TYPING_HANDLER_SENDINGS)
+@pytest.mark.asyncio
+async def test_positive_new_chat_message_typing_handler(handler_kwargs: dict,
+                                                        expected_sendings,
+                                                        ) -> None:
+    await _test_positive_handler_and_sendings(new_chat_message_typing, handler_kwargs, expected_sendings)
