e9dab468c5d44e85b14f2b77a175ec9d1686cc54
danil <danil.shevelev.2004@mail.ru>
2024-02-27 23:17:35 +0500

    Новый тип сообщений - "chatMessageWasRead"

--- websocket_/messages.py
+++ websocket_/messages.py
@@ -37,6 +37,7 @@ class MessageType(StrEnum):
 
     INTERLOCUTORS_ONLINE_STATUSES = 'interlocutorsOnlineStatuses'
     ONLINE_STATUS_TRACING_ADDING = 'onlineStatusTracingAdding'
+    CHAT_MESSAGE_WAS_READ = 'chatMessageWasRead'
     NEW_CHAT = 'newChat'
     NEW_CHAT_MESSAGE = 'newChatMessage'
     NEW_CHAT_MESSAGE_TYPING = 'newChatMessageTyping'
