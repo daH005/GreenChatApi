c9e3ddd6b97acc58f99f9cb463833e046ebf6467
danil <danil.shevelev.2004@mail.ru>
2024-04-28 11:31:06 +0500

    [#40] Упростил модель валидации `UserJSONValidator`

--- http_/validation.py
+++ http_/validation.py
@@ -2,15 +2,13 @@ from __future__ import annotations
 from pydantic import (
     BaseModel,
     constr,
-    Field,
+    conint,
     validate_email,
     field_validator,
     ValidationError,
 )
 from flask import request, abort
 from http import HTTPStatus
-
-from api.json_ import JSONKey
 
 __all__ = (
     'RequestDataHandlerMixin',
@@ -30,23 +28,8 @@ class RequestDataHandlerMixin(BaseModel):
 
 class UserJSONValidator(RequestDataHandlerMixin):
 
-    first_name: constr(min_length=1, max_length=100) = Field(alias=JSONKey.FIRST_NAME)
-    last_name: constr(min_length=1, max_length=100) = Field(alias=JSONKey.LAST_NAME)
-    username: constr(min_length=5, max_length=100)
-    password: constr(min_length=10, max_length=50)
     email: constr(min_length=1, max_length=200)
-    code: int
-
-    @field_validator('first_name', 'last_name', 'username', 'password', 'email')  # noqa: from pydantic doc
-    @classmethod
-    def _validate_many(cls, data: str) -> str:
-        cls._check_spaces_and_line_breaks_in_str(data)
-        return data
-
-    @staticmethod
-    def _check_spaces_and_line_breaks_in_str(str_: str) -> None:
-        if ' ' in str_ or '\n' in str_:
-            raise AssertionError('All fields (except code) must not contain spaces or line breaks')
+    code: conint(ge=1000)
 
     @field_validator('email')  # noqa: from pydantic doc
     @classmethod
