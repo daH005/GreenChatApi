ab71c25f01ff7d47651fa2e3cc38b9d9c11cb985
danil <danil.shevelev.2004@mail.ru>
2024-06-08 19:28:01 +0500

    added `JsonDictType` type alias

--- _tests/common.py
+++ _tests/common.py
@@ -1,9 +1,13 @@
 from datetime import datetime
+from typing import TypeAlias
 
 __all__ = (
     'COMMON_DATETIME',
+    'JsonDictType',
 )
 
 COMMON_DATETIME: datetime = datetime(
     year=2024, month=10, day=10,
 )
+
+JsonDictType: TypeAlias = dict[str, str | int | bool]
