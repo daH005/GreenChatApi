8eb3c4d5e03ffe33f0bb80911f8a52e14454d1ca
dah005 <danil.shevelev.2004@mail.ru>
2024-09-06 12:47:04 +0500

    fix of function (returns and exceptions)

--- ssl_context.py
+++ ssl_context.py
@@ -10,11 +10,16 @@ __all__ = (
 
 def get_ssl_context() -> SSLContext | None:
     ssl_path: Path = BASE_DIR.joinpath('ssl_')
-    ssl_context = None
-    if ssl_path.exists():
-        ssl_context = create_default_context(Purpose.CLIENT_AUTH)
+    if not ssl_path.exists():
+        return
+
+    ssl_context = create_default_context(Purpose.CLIENT_AUTH)
+    try:
         ssl_context.load_cert_chain(
             certfile=ssl_path.joinpath('certificate.crt'),
             keyfile=ssl_path.joinpath('private.key'),
         )
+    except FileNotFoundError:
+        return
+
     return ssl_context
