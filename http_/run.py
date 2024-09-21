920956ca1d33ba3c0f186a585b054c2dd5b78efd
dah005 <danil.shevelev.2004@mail.ru>
2024-09-21 22:34:25 +0500

    1. `run_http` -> `run_http_with_gunicorn` 2. added `run_default_http`

--- http_/run.py
+++ http_/run.py
@@ -1,14 +1,17 @@
 from typing import NoReturn
 from subprocess import run as run_subprocess
 
+from common.ssl_context import get_ssl_context
 from config import HOST, HTTP_PORT as PORT
+from http_.app import app
 
 __all__ = (
-    'run_http',
+    'run_http_with_gunicorn',
+    'run_default_http',
 )
 
 
-def run_http() -> NoReturn:
+def run_http_with_gunicorn() -> NoReturn:
     run_subprocess([
         'gunicorn',
         f'--certfile=ssl_/certificate.crt', f'--keyfile=ssl_/private.key',
@@ -16,3 +19,7 @@ def run_http() -> NoReturn:
         '-b', f'{HOST}:{PORT}',
         'http_.app_for_wsgi:app',
     ])
+
+
+def run_default_http() -> NoReturn:
+    app.run(HOST, PORT, ssl_context=get_ssl_context())
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from threading import Thread
 
-from http_.run import run_http
+from http_.run import run_http_with_gunicorn
 from http_.email.run import run_celery
 from websocket_.run import run_websocket
 
 if __name__ == '__main__':
-    for func in (run_websocket, run_http, run_celery):
+    for func in (run_websocket, run_http_with_gunicorn, run_celery):
         Thread(target=func).start()
 
     while True:
