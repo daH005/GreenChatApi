6393d521dea479f636ffc564f9192979220f1c27
Danil <danil.shevelev.2004@mail.ru>
2024-12-31 14:05:46 +0500

    [refactoring][tests] urn -> url

--- _tests/test_http.py
+++ _tests/test_http.py
@@ -26,37 +26,37 @@ def setup_module() -> None:
 
 @pytest.mark.parametrize('test_endpoint_kwargs', CHECK_EMAIL_TEST_ENDPOINT_KWARGS)
 def test_check_email(test_endpoint_kwargs: dict) -> None:
-    _test_get_endpoint(urn='/user/login/email/check', **test_endpoint_kwargs)
+    _test_get_endpoint(url='/user/login/email/check', **test_endpoint_kwargs)
 
 
 @pytest.mark.parametrize('test_endpoint_kwargs', LOGIN_TEST_ENDPOINT_KWARGS)
 def test_login(test_endpoint_kwargs: dict) -> None:
-    _test_post_or_put_endpoint(urn='/user/login', method='POST', **test_endpoint_kwargs)
+    _test_post_or_put_endpoint(url='/user/login', method='POST', **test_endpoint_kwargs)
 
 
 @pytest.mark.parametrize('test_endpoint_kwargs', REFRESH_ACCESS_TEST_ENDPOINT_KWARGS)
 def test_refresh_access(test_endpoint_kwargs: dict) -> None:
-    _test_post_or_put_endpoint(urn='/user/refreshAccess', method='POST', **test_endpoint_kwargs)
+    _test_post_or_put_endpoint(url='/user/refreshAccess', method='POST', **test_endpoint_kwargs)
 
 
 @pytest.mark.parametrize('test_endpoint_kwargs', USER_INFO_TEST_ENDPOINT_KWARGS)
 def test_user_info(test_endpoint_kwargs: dict) -> None:
-    _test_get_endpoint(urn='/user/info', **test_endpoint_kwargs)
+    _test_get_endpoint(url='/user/info', **test_endpoint_kwargs)
 
 
 @pytest.mark.parametrize('test_endpoint_kwargs', USER_INFO_EDIT_TEST_ENDPOINT_KWARGS)
 def test_user_info_edit(test_endpoint_kwargs: dict) -> None:
-    _test_post_or_put_endpoint(urn='/user/info/edit', method='PUT', **test_endpoint_kwargs)
+    _test_post_or_put_endpoint(url='/user/info/edit', method='PUT', **test_endpoint_kwargs)
 
 
 @pytest.mark.parametrize('test_endpoint_kwargs', USER_CHATS_TEST_ENDPOINT_KWARGS)
 def test_user_chats(test_endpoint_kwargs: dict) -> None:
-    _test_get_endpoint(urn='/user/chats', **test_endpoint_kwargs)
+    _test_get_endpoint(url='/user/chats', **test_endpoint_kwargs)
 
 
 @pytest.mark.parametrize('test_endpoint_kwargs', CHAT_HISTORY_TEST_ENDPOINT_KWARGS)
 def test_chat_history(test_endpoint_kwargs: dict) -> None:
-    _test_get_endpoint(urn='/chat/history', **test_endpoint_kwargs)
+    _test_get_endpoint(url='/chat/history', **test_endpoint_kwargs)
 
 
 def _test_get_endpoint(data: HeadersOrQueryArgsType | None = None,
@@ -71,7 +71,7 @@ def _test_post_or_put_endpoint(data: JsonDictType | None = None,
     _test_endpoint(**kwargs, json_dict=data)
 
 
-def _test_endpoint(urn: str,
+def _test_endpoint(url: str,
                    method: str,
                    expected_response_status_code: HTTPStatus,
                    headers: HeadersOrQueryArgsType | None = None,
@@ -80,7 +80,7 @@ def _test_endpoint(urn: str,
                    expected_response_json_dict: JsonDictType | None = None,
                    ) -> None:
     response: TestResponse = _test_client.open(
-        urn,
+        url,
         method=method,
         query_string=query_args,
         headers=headers,
