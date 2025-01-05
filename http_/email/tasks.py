8d4053249cb1af00b3ae4cccdab72efa271021ce
Danil <danil.shevelev.2004@mail.ru>
2025-01-05 23:50:21 +0500

    SMTPAuthenticationError + refactoring

--- http_/email/tasks.py
+++ http_/email/tasks.py
@@ -1,7 +1,8 @@
-from smtplib import SMTP_SSL
+from smtplib import SMTP_SSL, SMTPAuthenticationError
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from celery import Celery
+from typing import Final
 
 from config import REDIS_URL
 from config import (
@@ -18,16 +19,25 @@ __all__ = (
 
 app: Celery = Celery(broker=REDIS_URL, broker_connection_retry_on_startup=True)
 
+_MESSAGE_TITLE: Final[str] = 'GreenChat - Подтверждение почты'
+_MESSAGE_TEXT_TEMPLATE: Final[str] = 'Ваш код подтверждения: {}'
+
 
 @app.task(name='send_code_task', max_retries=0)
 def send_code_task(to: str,
                    code: int | str,
                    ) -> None:
     msg = MIMEMultipart()
-    msg['Subject'] = 'GreenChat - Подтверждение почты'
+    msg['Subject'] = _MESSAGE_TITLE
     msg['From'] = EMAIL
-    msg.attach(MIMEText('Ваш код подтверждения: ' + str(code)))
+    msg.attach(MIMEText(_MESSAGE_TEXT_TEMPLATE.format(str(code))))
 
-    with SMTP_SSL(host=SMTP_HOST, port=SMTP_PORT) as server:
-        server.login(user=EMAIL, password=EMAIL_PASSWORD)
-        server.sendmail(from_addr=EMAIL, to_addrs=to, msg=msg.as_string())
+    try:
+        connection: SMTP_SSL = SMTP_SSL(SMTP_HOST, SMTP_PORT)
+    except SMTPAuthenticationError:
+        return
+
+    connection.login(EMAIL, EMAIL_PASSWORD)
+    connection.sendmail(EMAIL, to, msg.as_string())
+
+    connection.close()
